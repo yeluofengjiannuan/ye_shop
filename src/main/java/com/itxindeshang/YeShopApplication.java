71ae998e1859a33a5a2026cba5a0bbdd8e73a2d1
叶落风渐暖 <2747959947@qq.com>
2026-07-26 00:20:54 +0800

    Initial commit

---
+++ src/main/java/com/itxindeshang/YeShopApplication.java
@@ -0,0 +1,13 @@
+package com.itxindeshang;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YeShopApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(YeShopApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/itxindeshang/YeShopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.itxindeshang;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YeShopApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
