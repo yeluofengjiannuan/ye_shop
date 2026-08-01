6723f770d7b6678920ccf2d2da2d48cbbba4bca5
叶落风渐暖 <2747959947@qq.com>
2026-08-01 22:18:59 +0800

    feat: 实现用户注册与登录, 初始化项目脚手架，集成 Spring Boot/MyBatis-Plus/Redis 核心环境

--- src/main/java/com/itxindeshang/YeShopApplication.java
+++ src/main/java/com/itxindeshang/YeShopApplication.java
@@ -1,9 +1,19 @@
 package com.itxindeshang;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
+import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @SpringBootApplication
+@MapperScan("com.itxindeshang.mapper")
+@EnableScheduling//启动定时任务
+@EnableTransactionManagement
+@EnableAspectJAutoProxy(exposeProxy = true)
+@EnableAsync//异步任务
 public class YeShopApplication {
 
     public static void main(String[] args) {
