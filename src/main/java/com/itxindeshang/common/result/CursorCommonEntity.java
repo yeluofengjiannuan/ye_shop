95928a9d82e1f705306307f344e7b476d6d14500
叶落风渐暖 <2747959947@qq.com>
2026-08-18 21:21:16 +0800

    feat: 增加游标返回实体注释

--- src/main/java/com/itxindeshang/common/result/CursorCommonEntity.java
+++ src/main/java/com/itxindeshang/common/result/CursorCommonEntity.java
@@ -13,13 +13,23 @@ import lombok.experimental.Accessors;
 @NoArgsConstructor
 @Builder
 public class CursorCommonEntity {
-
+    /**
+     * 查询种类
+     */
     @NotNull
     private String sortType;
 
+    /**
+     * 末尾查询值
+     */
     private String sortValue ;
 
+    /**
+     *  末尾查询 id"
+     */
     private Long sortId;
-
+    /**
+     * 查询数量
+     */
     private Integer querySize = 20;
 }
