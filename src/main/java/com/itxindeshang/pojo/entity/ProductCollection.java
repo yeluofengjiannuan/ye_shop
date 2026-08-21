3826e258cfb700296baa24ea4c8c62bfb7ec71ed
叶落风渐暖 <2747959947@qq.com>
2026-08-22 00:43:12 +0800

    feat(product): 新建商品收藏类实体

---
+++ src/main/java/com/itxindeshang/pojo/entity/ProductCollection.java
@@ -0,0 +1,47 @@
+package com.itxindeshang.pojo.entity;
+
+import com.baomidou.mybatisplus.annotation.*;
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.itxindeshang.common.constant.DatePatternConstants;
+import lombok.Data;
+import org.springframework.format.annotation.DateTimeFormat;
+
+import java.io.Serial;
+import java.io.Serializable;
+import java.time.LocalDateTime;
+
+/**
+ * 商品收藏表 实体类
+ * @TableName collection
+ */
+@Data
+@TableName(value = "collectio")
+public class ProductCollection implements Serializable {
+    @Serial
+    private static final long serialVersionUID = 1L;
+    /**
+     * 收藏ID（主键）
+     */
+    @TableId(type = IdType.AUTO)
+    private Long id;
+
+    /**
+     * 关联用户ID（外键）
+     */
+    @TableField(value = "user_id")
+    private Long userId;
+
+    /**
+     * 关联商品ID（外键）
+     */
+    @TableField(value = "product_id")
+    private Long productId;
+
+    /**
+     * 收藏时间
+     */
+    @TableField(value = "create_time", fill = FieldFill.INSERT)
+    @DateTimeFormat(pattern = DatePatternConstants.DATE_TIME_FORM)
+    @JsonFormat(pattern = DatePatternConstants.DATE_TIME_FORM)
+    private LocalDateTime createTime;
+}
